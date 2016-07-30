a4b25e6d5cbc91cc6a6257f9cf8111fffc3da863
Web Team <webteam@almostanything.com.au>
2016-07-30 12:19:24 +1000

    adding some basic components

---
+++ src/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello");
+    return $response;
+});
+$app->run();
